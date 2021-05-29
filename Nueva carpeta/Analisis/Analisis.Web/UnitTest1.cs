39b2cbd9c0668696595290e0d145527218a637c6
Gerstirling <85002519+Gerstirling@users.noreply.github.com>
2021-05-29 10:48:28 -0600

    Add files via upload

---
+++ Nueva carpeta/Analisis/Analisis.Web/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Analisis.Web
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
